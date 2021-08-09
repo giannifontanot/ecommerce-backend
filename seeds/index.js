79ffeb62611f11b51ca115ea3b2017626481cd32
giannifontanot <giannifontanot@gmail.com>
2021-08-09 17:16:21 -0500

    Eliminated extra comments to make it clearer.

--- seeds/index.js
+++ seeds/index.js
@@ -1,26 +1,27 @@
+// Sequelize modules required
 const seedCategories = require('./category-seeds');
 const seedProducts = require('./product-seeds');
 const seedTags = require('./tag-seeds');
 const seedProductTags = require('./product-tag-seeds');
 
+// Connection to the database
 const sequelize = require('../config/connection');
 
-const seedAll = async () => {
-  await sequelize.sync({ force: true });
-  console.log('\n----- DATABASE SYNCED -----\n');
-  await seedCategories();
-  console.log('\n----- CATEGORIES SEEDED -----\n');
+// Verify the connection to the database
+sequelize.authenticate()
+    .then(() => console.log('Connection established successfully'))
+    .catch(err => console.error('Unable to connect to database.', err));
 
-  await seedProducts();
-  console.log('\n----- PRODUCTS SEEDED -----\n');
+// Define the function to export for the server to use when starting
+exports.seedAll = async () => {
+    await sequelize.sync({force: true});
 
-  await seedTags();
-  console.log('\n----- TAGS SEEDED -----\n');
+    await seedCategories();
 
-  await seedProductTags();
-  console.log('\n----- PRODUCT TAGS SEEDED -----\n');
+    await seedProducts();
 
-  process.exit(0);
+    await seedTags();
+
+    await seedProductTags();
+
 };
-
-seedAll();
