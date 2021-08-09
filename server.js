87666130c9c6dfa48e71f89341b440209d11cbf5
giannifontanot <giannifontanot@gmail.com>
2021-08-09 17:19:03 -0500

    Modified to include the seeding of the database.

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
-const routes = require('./routes');
-// import sequelize connection
+
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
+const {sequelize} = require('./models/index');
 
+// Define the middleware that gets into the server
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
+app.use(express.urlencoded({extended: true}));
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+
+// RUN FOREST, RUN!!!
+sequelize.sync({force: true}).then(r => {
+
+    return seed.seedAll();
+    app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+
+}).then(() => {
+
+
 });
