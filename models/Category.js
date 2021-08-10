b7565f4829712db705b01cecc7f41e17dacc0bc6
giannifontanot <giannifontanot@gmail.com>
2021-08-10 16:04:36 -0500

    Database name.

--- models/Category.js
+++ models/Category.js
@@ -1,7 +1,5 @@
 'use strict';
-const {
-    Model
-} = require('sequelize');
+const {Model} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
     class Category extends Model {
         /**
@@ -9,16 +7,18 @@ module.exports = (sequelize, DataTypes) => {
          * This method is not a part of Sequelize lifecycle.
          * The `models/index` file will call this method automatically.
          */
-        static associate(models) {
+        static associate({Product}) {
             // define association here
+            this.belongsTo(Product, {foreignKey:'category_id'});
         }
     };
     Category.init({
-        id: {type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true},
+        category_id: {type: DataTypes.INTEGER, allowNull: false, primaryKey: true, autoIncrement: true},
         category_name: {type: DataTypes.STRING, allowNull: false}
     }, {
         sequelize,
-        modelName: 'Category',
+        modelName: 'Catego',
+        tableName: 'CATEGORIES',
     });
     return Category;
 };
