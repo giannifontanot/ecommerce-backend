43afa94d7bcc8902a4c5b7b227e6bfd93095fc7a
giannifontanot <giannifontanot@gmail.com>
2021-08-10 16:10:34 -0500

    Deleted the ability to seed the db.

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ const {sequelize} = require('./models/index');
 // Define the middleware that gets into the server
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(routes);
+//app.use(routes);
 
 
 // RUN FOREST, RUN!!!
 sequelize.sync({force: true}).then(r => {
 
-    return seed.seedAll();
+    //return seed.seedAll();
     app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 
 }).then(() => {
